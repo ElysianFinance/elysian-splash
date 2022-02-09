ac91b2b5ae97c088a221c797ecdb0d29de80feb6
kingluxor <luxor@elysian.finance>
2022-02-09 19:34:43 -0400

    web3 compatibility

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,10 +4,27 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 const path = require('path');
+const webpack = require("webpack");
+
 exports.onCreateWebpackConfig = ({ stage, actions }) => {
   actions.setWebpackConfig({
+    plugins: [
+      new webpack.ProvidePlugin({
+          Buffer: [require.resolve("buffer/"), "Buffer"],
+      }),
+    ],
     resolve: {
       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      fallback: {
+        "crypto": false,
+        "stream": require.resolve("stream-browserify"),
+        "assert": false,
+        "util": false,
+        "http": false,
+        "https": false,
+        "os": false
+    },
     },
   });
 };
+ 
