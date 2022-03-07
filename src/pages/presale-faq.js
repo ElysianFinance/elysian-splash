5f669a068f495767c3021ed8e4b672380e071d69
kingluxor <luxor@elysian.finance>
2022-03-07 17:43:53 -0400

    fluid

--- src/pages/presale-faq.js
+++ src/pages/presale-faq.js
@@ -4,14 +4,13 @@ import Seo from 'components/seo';
 import Banner from 'sections/banner';
 import Layout from 'components/layout';
 import { jsx, Flex, Container, Box, Heading, Text, Button, Link } from 'theme-ui';
-import {isMobile} from 'react-device-detect';
+import { useBreakpoint } from 'gatsby-plugin-breakpoints';
 
 import 'rc-drawer/assets/index.css';
 import 'react-modal-video/css/modal-video.min.css';
-//39419b
-export default function IndexPage() {
 
-  let paddingTop = isMobile ? '20%' : '10%';
+export default function IndexPage() {
+  const breakpoints = useBreakpoint();
 
   const addTokenToMetamask = async () => {
 
@@ -46,7 +45,96 @@ export default function IndexPage() {
       }
     }
   }
-  
+  /*<>
+  {breakpoints.xs ? 
+<Box as="section" id="banner" style={{paddingTop: "20%"}}> : 
+<Box as="section" id="banner" style={{paddingTop: "10%"}}>}
+</>*/
+
+const content = () => {
+  return (
+    <table >
+    <tr>  
+      <td>
+        <h3>{"When is the presale?"}</h3>
+      </td>
+      <td>
+        <p>{"The presale has begun and will last until all tokens are exhausted."}</p>
+      </td>
+    </tr>
+    <tr>  
+      <td>
+        <h3>{"How do I purchase pLYS token?"}</h3>
+      </td>
+      <td>
+        <p>{"Accepted currencies are ETH, BNB, DAI, USDC, USDT and sUSD."}</p>
+      </td>
+    </tr>
+    <tr>  
+      <td>
+        <h3>{"On which blockchain is the presale happening?"}</h3>
+      </td>
+      <td>
+        <p>{"The presale will take place simultaneously on Ethereum and Binance Smart Chain (BSC). If you buy on BSC, you will be able to redeem your tokens on Ethereum."}</p>
+      </td>
+    </tr>
+    <tr>  
+      <td>
+        <h3>{"What is pLYS price during the private sale?"}</h3>
+      </td>
+      <td>
+        <p>{"The price during the private sale is $0.01 (US Dollars)."}</p>
+      </td>
+    </tr>   
+    <tr>  
+      <td>
+        <h3>{"Is there a minimum investment amount during the token sale?"}</h3>
+      </td>
+      <td>
+        <p>{"There is no minimum, however there is a maximum number of tokens allocated. The allocation is calculated as the total amount of pLYS available divided by the amount of participants and will vary if participants do not buy their whole allocation."}</p>
+      </td>
+    </tr>  
+    <tr>  
+      <td>
+        <h3>{"What happens if I don't buy my whole allocation?"}</h3>
+      </td>
+      <td>
+        <p>{"You won't have a second chance to buy as the amount unsold gets redistributed to the pool of investors and you lose access to the whitelist."}</p>
+      </td>
+    </tr>                 
+    <tr>  
+      <td>
+        <h3>{"Are there any restrictions on who can participate in the token sale?"}</h3>
+      </td>
+      <td>
+        <p>{"If you are a citizen of any US or UK sanctioned countries you shall not participate in this sale. In addition, we are not able to do business with individuals who are on sanctions lists or who are in countries on sanctions lists (e.g. North Korea, Iran, etc)."}</p>
+      </td>
+    </tr>    
+    <tr>  
+      <td>
+        <h3>{"How do I confirm my contribution was received?"}</h3>
+      </td>
+      <td>
+        <p>{"Your funds will be reflected on your wallet as soon as your contribution is received and has been confirmed on the network. If you don’t receive the tokens, contact an admin on Telegram or Discord."}
+        <br /><span style={{color:"#905bc4"}}><a onClick={addTokenToMetamask}>Add token to Metamask</a></span></p>
+      </td>
+    </tr> 
+    <tr>  
+      <td>
+      </td>
+      <td>
+      <Box sx={styles.btnWrap}>
+        <Link href={"https://launch.elysian.finance"} target="_new" sx={styles.btn}>
+          {"Go to launchpad"}
+        </Link>
+      </Box>
+      </td>
+    </tr> 
+  </table>
+
+  )
+}
+console.log(breakpoints)
   return (
     <StickyProvider>
       <Layout>
@@ -54,99 +142,32 @@ export default function IndexPage() {
           title="Elysian Finance"
           description="Elysian Finance is a next gen reserve backed token for DeFi 3.0 and the decentralized web."
         />
-        <Box as="section" id="banner" style={{paddingTop:paddingTop}}>
+         {breakpoints.xs || breakpoints.sm ? 
+         <Box as="section" id="banner" style={{paddingTop: "20%"}}>  
           <Container sx={styles.container}>
             <Box sx={styles.content}>
               <Heading as="h1"> 
                 <span style={{color:"#905bc4", fontSize:"36px"}}>{"Presale FAQ"}</span>{" "}<br/>
                 </Heading>
-              <br />
-              <table >
-                <tr>  
-                  <td>
-                    <h3>{"When is the presale?"}</h3>
-                  </td>
-                  <td>
-                    <p>{"The presale has begun and will last until all tokens are exhausted."}</p>
-                  </td>
-                </tr>
-                <tr>  
-                  <td>
-                    <h3>{"How do I purchase pLYS token?"}</h3>
-                  </td>
-                  <td>
-                    <p>{"Accepted currencies are ETH, BNB, DAI, USDC, USDT and sUSD."}</p>
-                  </td>
-                </tr>
-                <tr>  
-                  <td>
-                    <h3>{"On which blockchain is the presale happening?"}</h3>
-                  </td>
-                  <td>
-                    <p>{"The presale will take place simultaneously on Ethereum and Binance Smart Chain (BSC). If you buy on BSC, you will be able to redeem your tokens on Ethereum."}</p>
-                  </td>
-                </tr>
-                <tr>  
-                  <td>
-                    <h3>{"What is pLYS price during the private sale?"}</h3>
-                  </td>
-                  <td>
-                    <p>{"The price during the private sale is $0.01 (US Dollars)."}</p>
-                  </td>
-                </tr>   
-                <tr>  
-                  <td>
-                    <h3>{"Is there a minimum investment amount during the token sale?"}</h3>
-                  </td>
-                  <td>
-                    <p>{"There is no minimum, however there is a maximum number of tokens allocated. The allocation is calculated as the total amount of pLYS available divided by the amount of participants and will vary if participants do not buy their whole allocation."}</p>
-                  </td>
-                </tr>  
-                <tr>  
-                  <td>
-                    <h3>{"What happens if I don't buy my whole allocation?"}</h3>
-                  </td>
-                  <td>
-                    <p>{"You won't have a second chance to buy as the amount unsold gets redistributed to the pool of investors and you lose access to the whitelist."}</p>
-                  </td>
-                </tr>                 
-                <tr>  
-                  <td>
-                    <h3>{"Are there any restrictions on who can participate in the token sale?"}</h3>
-                  </td>
-                  <td>
-                    <p>{"If you are a citizen of any US or UK sanctioned countries you shall not participate in this sale. In addition, we are not able to do business with individuals who are on sanctions lists or who are in countries on sanctions lists (e.g. North Korea, Iran, etc)."}</p>
-                  </td>
-                </tr>    
-                <tr>  
-                  <td>
-                    <h3>{"How do I confirm my contribution was received?"}</h3>
-                  </td>
-                  <td>
-                    <p>{"Your funds will be reflected on your wallet as soon as your contribution is received and has been confirmed on the network. If you don’t receive the tokens, contact an admin on Telegram or Discord."}
-                    <br /><span style={{color:"#905bc4"}}><a onClick={addTokenToMetamask}>Add token to Metamask</a></span></p>
-                  </td>
-                </tr> 
-                <tr>  
-                  <td>
-                  </td>
-                  <td>
-                  <Box sx={styles.btnWrap}>
-                    <Link href={"https://launch.elysian.finance"} target="_new" sx={styles.btn}>
-                      {"Go to launchpad"}
-                    </Link>
-                  </Box>
-                  </td>
-                </tr> 
-              </table>
-
-
+                {content()}
             </Box>
  
           </Container>
-        </Box> 
-      </Layout>
-    </StickyProvider>
+        </Box>      
+         :
+            <Box as="section" id="banner" style={{paddingTop: "10%"}}>  
+                <Container sx={styles.container}>
+               <Box sx={styles.content}>
+                 <Heading as="h1"> 
+                   <span style={{color:"#905bc4", fontSize:"36px"}}>{"Presale FAQ"}</span>{" "}<br/>
+                   </Heading>
+                   {content()}
+               </Box>
+             </Container>
+           </Box> 
+           }
+       </Layout>
+    </StickyProvider> 
   );
 }
 
